3e159b616a6feffcd8ac18d8be5c5372a63f2428
leaf-yezi <934912625@qq.com>
2018-12-16 19:17:26 +0800

    更新thinkphp快捷创建控制器模版

--- src/commands/thinkphpTemplateCommand.ts
+++ src/commands/thinkphpTemplateCommand.ts
@@ -1,4 +1,6 @@
-
+import vscode = require("vscode");
+import fs = require("fs");
+import path = require("path");
 
 /**
  * Main command to create thinkphp project class file.
@@ -7,8 +9,25 @@
  * @param {*} args
  * @returns
  */
-export function run( args: any){
+export function run(args: any) {
 
+    let targetFolder = args ? args.fsPath : vscode.workspace.rootPath + '/Application/Admin/Controller'
+    if (!fs.existsSync(targetFolder)) return vscode.window.showInformationMessage('The project controller directory was not found')
+    let inputOptions = <vscode.InputBoxOptions>{
+        prompt: "Please enter the desired file name",
+        value: "",
+        validateInput: val => {
+            if (/^[A-Z]\w*$/.test(val)) return null
+            return "Invalidata controller name"
+        }
+    }
 
-    
+    // ask for filename
+    vscode.window.showInputBox(inputOptions).then(name => {
+        // create file temaplate
+        let model = path.basename(path.dirname(targetFolder))
+        if (!model) return vscode.window.showInformationMessage('The project model directory was not found')
+        fs.writeFileSync(path.join(targetFolder, `${name}Controller.class.php`), "<?php\nnamespace " + model + "\\Controller;\nuse Think\\Controller;\nclass " + name + "Controller extends Controller {\n    public function index(){\n    }\n}")
+    })
+
 }
